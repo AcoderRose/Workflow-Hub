94fde5a1f30c8af06891fd0c0349ec685cf68e0b
AcoderRose <addyro96@att.net>
2024-04-30 20:25:55 -0700

    Worked on index.html created a form with labels and inputs for inputting data for the task, as well as messed with javascript a bit but still not working correctly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,29 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    // 
+    // make cards draggable
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    // store values of the inputs
+    const taskTitle = document.getElementById("task-title").value;
+    const taskDueDate = document.getElementById("task-due-date").value;
+    const taskDescription = document.getElementById("task-description").value;
 
-}
+    // add new task to task list
+    taskList.push({
+        title:taskTitle,
+        date:taskDueDate,
+        description: taskDescription,
+    })
+    // update local storage
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    // render the task list
+    renderTaskList();
+};
+
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
@@ -33,6 +49,14 @@ function handleDrop(event, ui) {
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+$(document).ready(function (event) {
 
+    event.preventDefault();
+    // render the task list
+    renderTaskList();
+   // add an event listener for the save task button
+    document.getElementById('saveTaskButton').addEventListener('click', handleAddTask);
 });
+
+
+
